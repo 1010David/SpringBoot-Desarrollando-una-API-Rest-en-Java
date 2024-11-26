21f6b00bc8f8fec3efaf6edc7a45e8a24e0436e8
David <j.david.beltran.m@gmail.com>
2024-11-26 18:00:24 -0500

    Migraciones flyway

--- src/main/java/com/aluraoracle_voll/api/medico/DatosRegistroMedico.java
+++ src/main/java/com/aluraoracle_voll/api/medico/DatosRegistroMedico.java
@@ -1,8 +1,26 @@
 package com.aluraoracle_voll.api.medico;
 
+import com.aluraoracle_voll.api.direccion.DatosDireccion;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Pattern;
+
 public record DatosRegistroMedico(
+
+        @NotBlank
         String nombre,
+        @NotBlank
+        @Email
         String email,
+        @NotBlank
+        @Pattern(regexp = "\\d{7,12}")
+        String telefono,
+        @NotBlank
+        @Pattern(regexp = "\\d{4,6}")
         String documento,
-        Especialidad especialidad) {
+        @NotBlank
+        Especialidad especialidad,
+        @NotNull
+        DatosDireccion direccion) {
 }
